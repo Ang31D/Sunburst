29b34afd345b968d094834aabdb1f369254bd200
Ang31D <53995136+Ang31D@users.noreply.github.com>
2020-12-24 01:26:26 +0100

    Update decode_b64.sh

--- tools/decode_b64.sh
+++ tools/decode_b64.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 script_pwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+tools_dir=$(echo "$script_pwd")
 
-echo "$1" | python3 $script_pwd/dencode.py
+echo "$1" | python3 $tools_dir/dencode.py

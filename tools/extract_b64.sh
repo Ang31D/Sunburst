8916e77b430f2cfc720e92765f3787349af78fe1
Ang31D <53995136+Ang31D@users.noreply.github.com>
2020-12-25 17:48:23 +0100

    Update extract_b64.sh

--- tools/extract_b64.sh
+++ tools/extract_b64.sh
@@ -2,6 +2,6 @@
 
 if [[ -p "/dev/stdin" ]]; then
    cat "/dev/stdin" | tr " " "\n" | grep -Eo 'Unzip\("(.+)"\)' | sed 's/Unzip("//g' | sed 's/")$//g'
-else
+elif [[ "${#1}" > 0 ]]; then
    cat "$1" | tr " " "\n" | grep -Eo 'Unzip\("(.+)"\)' | sed 's/Unzip("//g' | sed 's/")$//g'
 fi

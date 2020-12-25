02d1b2c130e0442a59fe3800616f0944b3517cce
Ang31D <53995136+Ang31D@users.noreply.github.com>
2020-12-25 08:40:40 +0100

    Update decode_b64.sh

--- tools/decode_b64.sh
+++ tools/decode_b64.sh
@@ -3,4 +3,8 @@
 script_pwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 tools_dir=$(echo "$script_pwd")
 
-echo "$1" | python3 $tools_dir/dencode.py
+if [[ -p "/dev/stdin" ]]; then
+   cat "/dev/stdin" | python3 $tools_dir/dencode.py
+else
+   echo "$1" | python3 $tools_dir/dencode.py
+fi

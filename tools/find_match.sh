7551e04fd968576bbce016830cd4c421ec901123
Ang31D <53995136+Ang31D@users.noreply.github.com>
2020-12-25 17:36:09 +0100

    Update find_match.sh

--- tools/find_match.sh
+++ tools/find_match.sh
@@ -3,15 +3,14 @@
 script_pwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 hashes_dir=$(echo "$script_pwd/../hashes")
 
-value=$(echo "$1")
-
 hash_file=$(echo "$hashes_dir/hashcat_team.cracked_hashes.txt")
 if [[ "${#2}" > 0 ]]; then
    hash_file=$(echo "$2")
 fi
 
-matches=$(grep -i "${value}" "$hash_file")
-
-if [[ "${#matches}" > 0 ]]; then
-   echo "${matches}"
+if [[ -p "/dev/stdin" ]]; then
+   cat "/dev/stdin" | while read value; do grep -i "${value}" "$hash_file"; done
+else
+   value=$(echo "$1")
+   grep -i "${value}" "$hash_file"
 fi

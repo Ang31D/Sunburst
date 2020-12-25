e41d31993dd7f4cecb640b9c4e1c5b45fcf23134
Ang31D <53995136+Ang31D@users.noreply.github.com>
2020-12-25 17:25:58 +0100

    Update lookup_hash.sh

--- tools/lookup_hash.sh
+++ tools/lookup_hash.sh
@@ -3,14 +3,17 @@
 script_pwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 hashes_dir=$(echo "$hash $script_pwd/../hashes")
 
-hash=$(echo "$1")
-
 hash_file=$(echo "$hashes_dir/hashcat_team.cracked_hashes.txt")
 if [[ "${#2}" > 0 ]]; then
    hash_file=$(echo "$2")
 fi
 
-match=$(grep -E "^${hash} | ${hash}$" $hash_file)
+if [[ -p "/dev/stdin" ]]; then
+   cat "/dev/stdin" | while read hash; do grep -E "^${hash} | ${hash}$" $hash_file; done
+else
+   hash=$(echo "$1")
+   match=$(grep -E "^${hash} | ${hash}$" $hash_file)
+fi
 
 if [[ "${#match}" > 0 ]]; then
    echo "${match}"

80a337001ef7671880f11a0f3ab272bdc3684c10
Ang31D <53995136+Ang31D@users.noreply.github.com>
2020-12-25 17:44:23 +0100

    Update hash_value.sh

--- tools/hash_value.sh
+++ tools/hash_value.sh
@@ -3,8 +3,10 @@
 script_pwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 tools_dir=$(echo "$script_pwd")
 
-value=$(echo "$1")
-
-hash=$(echo "$value" | python3 $tools_dir/dencode.py -H)
-
-echo "${hash} ${value}"
+if [[ -p "/dev/stdin" ]]; then
+   cat "/dev/stdin" | while read value; do hash=$(echo "$value" | python3 $tools_dir/dencode.py -H); echo "${hash} ${value}"; done
+else
+   value=$(echo "$1")
+   hash=$(echo "$value" | python3 $tools_dir/dencode.py -H)
+   echo "${hash} ${value}"
+fi

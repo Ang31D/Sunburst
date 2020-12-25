cd2dcb62a9f255cad6ed285aae63ed17e8603792
Ang31D <53995136+Ang31D@users.noreply.github.com>
2020-12-25 17:45:45 +0100

    Update find_match.sh

--- tools/find_match.sh
+++ tools/find_match.sh
@@ -10,7 +10,7 @@ fi
 
 if [[ -p "/dev/stdin" ]]; then
    cat "/dev/stdin" | while read value; do grep -i "${value}" "$hash_file"; done
-else
+elif [[ "${#1}" > 0 ]]; then
    value=$(echo "$1")
    grep -i "${value}" "$hash_file"
 fi
